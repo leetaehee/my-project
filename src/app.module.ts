b0c65db938c1fb4e79b237cd4920695bc26ab887
leetaehee <lastride25@naver.com>
2023-01-29 13:39:59 +0900

    유저서비스의 인터페이스 추가

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { UsersController } from './users/users.controller';
 
 @Module({
-  controllers: [AppController],
+  controllers: [AppController, UsersController],
 })
 export class AppModule {}
