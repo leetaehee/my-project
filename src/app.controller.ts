08a056e7eb391f32d15bba832d3d8fdc6dcca2fa
leetaehee <lastride25@naver.com>
2023-01-30 22:49:20 +0900

    dotenv 적용

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -6,6 +6,6 @@ export class AppController {
 
   @Get()
   getHello(): string {
-    return "Hello World!";
+    return process.env.DATABASE_HOST;
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import emailConfig from './config/emailConfig';
+import { validationSchema } from './config/validationSchema';
 import { AppController } from './app.controller';
 import { UsersModule } from './users/users.module';
 
 @Module({
   controllers: [AppController],
   providers: [],
-  imports: [UsersModule],
+  imports: [
+    UsersModule,
+    ConfigModule.forRoot({
+      envFilePath: [`${__dirname}/config/env/.${process.env.NODE_ENV}.env`],
+      load: [emailConfig],
+      isGlobal: true,
+      validationSchema,
+    }),
+  ],
 })
 export class AppModule {}
